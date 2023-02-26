7ecb8be8c44d3a5483b288beb8df338228c96ddf
Luis Solera <Luis Solera@LAPTOP-LUIS>
2023-02-25 22:48:24 -0600

    Añadidos Services, repositories y controllers nuevos

---
+++ ApplicationCore/Services/ServiceInformacion.cs
@@ -0,0 +1,24 @@
+﻿using Infraestructure.Models;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceInformacion : IServiceInformacion
+    {
+        public IEnumerable<Informacion> GetInformacion()
+        {
+            IRepositoryInformacion repository = new RepositoryInformacion();
+            return repository.GetInformacion();
+        }
+        public Informacion GetInformacionByID(int id)
+        {
+            IRepositoryInformacion repository = new RepositoryInformacion();
+            return repository.GetInformacionByID(id);
+        }
+    }
+}
