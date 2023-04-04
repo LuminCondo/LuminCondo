35bf7207cb9e245bd2cb7dae22cd231213552fd1
Israel Calvo Morera <icalvom@est.utn.ac.cr>
2023-04-04 01:52:04 -0600

    *** Ajax Asignacion de planes / Remocion de codigo viejo ***

--- LuminCondo/Controllers/GestionAsignacionPlanesController.cs
+++ LuminCondo/Controllers/GestionAsignacionPlanesController.cs
@@ -97,50 +97,6 @@ namespace Web.Controllers
             planCobros = _ServiceGestionPlanCobros.GetGestionPlanCobrosByID(Convert.ToInt32(id));
             return PartialView("_PartialViewDetails", planCobros);
         }
-        [CustomAuthorize((int)Roles.Administrador)]
-        // GET: GestionAsignacionPlanes/Details/5
-        public ActionResult Details(int? id)
-        {
-            IServiceGestionPlanCobros _ServiceGestionPlanCobros = new ServiceGestionPlanCobros();
-            GestionPlanCobros planCobros = null;
-            try
-            {
-                // Si va null
-                if (id == null)
-                {
-                    return RedirectToAction("Index");
-                }
-                planCobros = _ServiceGestionPlanCobros.GetGestionPlanCobrosByID(Convert.ToInt32(id));
-                if (planCobros == null)
-                {
-                    TempData["Message"] = "No existe el Plan solicitado";
-                    TempData["Redirect"] = "GestionPlanCobros";
-                    TempData["Redirect-Action"] = "Index";
-                    // Redireccion a la captura del Error
-                    return RedirectToAction("Default", "Error");
-                }
-                return View(planCobros);
-            }
-            catch (Exception ex)
-            {
-                // Salvar el error en un archivo 
-                Log.Error(ex, MethodBase.GetCurrentMethod());
-                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
-                TempData["Redirect"] = "GestionPlanCobros";
-                TempData["Redirect-Action"] = "Index";
-                // Redireccion a la captura del Error
-                return RedirectToAction("Default", "Error");
-            }
-
-        }
-        [CustomAuthorize((int)Roles.Administrador)]
-        // GET: GestionAsignacionPlanes/Create
-        public ActionResult Create()
-        {
-            ViewBag.IDResidencias = listaResidencias();
-            ViewBag.IDPlanes = listaPlanes();
-            return View();
-        }
 
         private SelectList listaResidencias(int idResidencia = 0)
         {
@@ -178,53 +134,6 @@ namespace Web.Controllers
             IServiceGestionPlanCobros _ServiceGestionPlanCobros = new ServiceGestionPlanCobros();
             IEnumerable<GestionPlanCobros> lista = _ServiceGestionPlanCobros.GetGestionPlanCobros();
             return new SelectList(lista, "IDPlan", "descripcion", idPlan);
-        }
-
-        [CustomAuthorize((int)Roles.Administrador)]
-        // GET: GestionAsignacionPlanes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            ViewBag.IDResidencias = listaResidencias();
-            ViewBag.IDPlanes = listaPlanes();
-            
-
-
-            IServiceGestionAsignacionPlanes _ServiceGestionAsignacionPlanes = new ServiceGestionAsignacionPlanes();
-            GestionAsignacionPlanes gestionAsignacionPlanes = null;
-
-            try
-            {
-                if (id == null)
-                {
-                    return RedirectToAction("Index");
-                }
-
-                gestionAsignacionPlanes = _ServiceGestionAsignacionPlanes.GetGestionAsignacionPlanesByID(Convert.ToInt32(id));
-
-                if (gestionAsignacionPlanes == null)
-                {
-                    TempData["Message"] = "No existe la asignacion solicitada";
-                    TempData["Redirect"] = "GestionAsignacionPlanes";
-                    TempData["Redirect-Action"] = "Index";
-                    // Redireccion a la captura del Error
-                    return RedirectToAction("Default", "Error");
-                }
-
-                ViewBag.IDResidencias = listaResidencias(gestionAsignacionPlanes.IDResidencia);
-                ViewBag.IDPlanes = listaPlanes(gestionAsignacionPlanes.IDPlan);
-                return View(gestionAsignacionPlanes);
-
-            }
-            catch (Exception ex)
-            {
-                // Salvar el error en un archivo 
-                Log.Error(ex, MethodBase.GetCurrentMethod());
-                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
-                TempData["Redirect"] = "Libro";
-                TempData["Redirect-Action"] = "IndexAdmin";
-                // Redireccion a la captura del Error
-                return RedirectToAction("Default", "Error");
-            }
         }
 
         public ActionResult BuscarHistorialGeneral(int? mes, int? anno, int? idResidencia)
